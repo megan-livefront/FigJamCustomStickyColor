374afc1e10c5ed54c0093241f87f08fc0a04ae84
meganpekarek <megan@livefront.com>
2024-03-17 13:47:39 -0500

    sticky note color changing

---
+++ code.ts
@@ -0,0 +1,27 @@
+figma.showUI(__html__);
+
+type Fills = typeof figma.mixed | readonly Paint[];
+
+/** Changes the color of all `STICKY` nodes within the selection to the given `color`. */
+figma.ui.onmessage = (msg: { type: string; color: string }) => {
+  if (msg.type === "change-color") {
+    const selectedNodes = figma.currentPage.selection;
+    selectedNodes.forEach((node) => {
+      if (node.type === "STICKY") {
+        const fills = cloneFills(node.fills);
+        if (Array.isArray(fills)) {
+          const color = msg.color;
+          const backgroundColor = color.includes("#") ? color : `#${color}`;
+          fills[0] = figma.util.solidPaint(backgroundColor, fills[0]);
+          node.fills = fills;
+        }
+      }
+    });
+
+    figma.closePlugin();
+  }
+};
+
+function cloneFills(fills: Fills): Fills {
+  return JSON.parse(JSON.stringify(fills));
+}
